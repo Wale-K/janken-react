97ae4b37e6a350aeb8ad8222f49d3fc3e580fdcf
Wale-K <olawale.k17@gmail.com>
2020-06-10 20:17:30 +0100

    Created the Title bar

---
+++ src/components/TitleBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+class TitleBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <h1>Rock Paper Scissors!</h1>
+      </>
+    );
+  }
+}
+
+export default TitleBar;
