7f51f449af9a006a4407162c944d11ded4180a15
Wale-K <olawale.k17@gmail.com>
2020-06-12 11:45:47 +0100

    When you click on any of the ChoicesImages it logs to console an outcome of win, loss or draw depending upon the selection the CPU has made (based of the cpuChoice variable).

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React from "react";
+import React from 'react';
 import TitleBar from "./components/TitleBar";
 import ScoreBoard from "./components/ScoreBoard";
 import Actions from "./components/Actions";
 import Message from "./components/DuelMessage";
 import ResetButton from "./components/ResetButton";
-import "./App.css";
+import './App.css';
+
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      userScore: 0,
-      cpuScore: 0,
-    };
+      userScore : 0,
+      cpuScore : 0
+    }
   }
   render() {
-    return (
-      <div className="App">
-        <TitleBar />
-        <ScoreBoard
-          userScore={this.state.userScore}
-          cpuScore={this.state.cpuScore}
-        />
-        <Actions />
-        <Message />
-        <ResetButton />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <TitleBar />
+     <ScoreBoard userScore = {this.state.userScore} cpuScore = {this.state.cpuScore} />
+     <Actions />
+    <Message />
+    <ResetButton />
+        
+    </div>
+  );
+}
 }
 
 export default App;
--- src/components/ChoicesImages.js
+++ src/components/ChoicesImages.js
@@ -1,16 +1,36 @@
 import React from "react";
 
+// const choices = ["rock", "paper", "scissors"];
+// const cpuChoice = choices[Math.floor(Math.random() * choices.length)];
+// If I use line 3 & 4 in the global scope the function below flops. When used locally it works. Why?
+
 class ChoicesImages extends React.Component {
   constructor(props) {
     super(props);
-this.handleChoice = this.handleChoice.bind(this)
+    this.handleChoice = this.handleChoice.bind(this);
   }
-handleChoice = () => {
-      console.log("hello")
-  }
+
+  handleChoice = (event) => {
+    const choices = ["rock", "paper", "scissors"];
+    const cpuChoice = choices[Math.floor(Math.random() * choices.length)]; 
+
+    if (event === cpuChoice) {
+      console.log(`Player: ${event}. CPU: ${cpuChoice} DRAW.`); // draw works.
+    // this.setState(this.props.userScore = this.props.userScore + 1) --> How can I change the state?
+
+    } else if (
+
+      (event === choices[0] && cpuChoice === "scissors") ||
+      (event === choices[1] && cpuChoice === "rock") ||
+      (event === choices[2] && cpuChoice === "paper")
+    ) {
+      console.log(`Player: ${event}. CPU: ${cpuChoice} WIN.`);
+    } else console.log(`Player: ${event}. CPU: ${cpuChoice} LOSE.`);
+  };
+
   render() {
     return (
-      <button onClick={this.handleChoice}>
+      <button onClick={() => this.handleChoice(this.props.name)}>
         <img src={this.props.image} alt={this.props.name} />
       </button>
     );
