668c0ecababbb669416564fc6c45a737eb37c063
Wale-K <olawale.k17@gmail.com>
2020-06-16 22:34:23 +0100

    The code has now been refactored so that all components except the app are functional.

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ChoicesImages from "./ChoicesImages";
+import {ChoicesImages} from "./ChoicesImages";
 import rock from "../images/rock.png";
 import paper from "../images/paper.png";
 import scissors from "../images/scissors.png";
@@ -10,48 +10,18 @@ const choices = [
   { name: "scissors", image: scissors },
 ];
 
-
-
-// export const Actions = (props) => {
-//   return (
-//     <ChoicesImages
-//       incrementDraw={props.incrementDraw}
-//       incrementCpu={props.incrementCpu}
-//       incrementUser={props.incrementUser}
-//       outcome={props.outcome}
-//       image={elem.image}
-//       name={elem.name}
-//       key={elem.name}
-//     />
-//   );
-// };
-
-
-
-class Actions extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  renderChoices = () => {
-    return choices.map((elem) => {
-      return (
-        <ChoicesImages
-          incrementDraw={this.props.incrementDraw}
-          incrementCpu={this.props.incrementCpu}
-          incrementUser={this.props.incrementUser}
-          outcome={this.props.outcome}
-          image={elem.image}
-          name={elem.name}
-          key={elem.name}
-        />
-      );
-    });
-  };
-
-  render() {
-    return this.renderChoices();
-  }
-}
-
-export default Actions;
+export const Actions = (props) => {
+  return choices.map((elem) => {
+    return (
+      <ChoicesImages
+        incrementDraw={props.incrementDraw}
+        incrementCpu={props.incrementCpu}
+        incrementUser={props.incrementUser}
+        outcome={props.outcome}
+        image={elem.image}
+        name={elem.name}
+        key={elem.name}
+      />
+    );
+  });
+};
--- src/components/ChoicesImages.js
+++ src/components/ChoicesImages.js
@@ -2,42 +2,72 @@ import React from "react";
 
 const choices = ["rock", "paper", "scissors"];
 
-class ChoicesImages extends React.Component {
-  constructor(props) {
-    super(props);
-    // this.handleChoice = this.handleChoice.bind(this);
-  }
+export const ChoicesImages = (props) => {
+   const handleChoice = () => {
+        const { name } = props;
+    
+        const cpuChoice = choices[Math.floor(Math.random() * choices.length)];
+    
+        if (name === cpuChoice) {
+            
+         return props.incrementDraw()
+          
+        } else if (
+          (name === "rock" && cpuChoice === "scissors") ||
+          (name === "paper" && cpuChoice === "rock") ||
+          (name === "scissors" && cpuChoice === "paper")
+        ) {
+         return props.incrementUser(name);
+         
+        } else
+      return  props.incrementCpu(name);
+      };
 
-  handleChoice = () => {
-    const { name } = this.props;
+      return (
+              <button onClick={handleChoice}>
+                <img src={props.image} alt={props.name} />
+              </button>
+            );
 
-    const cpuChoice = choices[Math.floor(Math.random() * choices.length)];
 
-    if (name === cpuChoice) {
-        
-     return this.props.incrementDraw()
-      
-    } else if (
-      (name === "rock" && cpuChoice === "scissors") ||
-      (name === "paper" && cpuChoice === "rock") ||
-      (name === "scissors" && cpuChoice === "paper")
-    ) {
-     return this.props.incrementUser(name);
-     
-    } else
-  return  this.props.incrementCpu(name);
-  };
-
-  // Depending on the outcome of the duel, a message needs to be displayed (win? loss? draw?)
-
-  render() {
-    return (
-      <button onClick={this.handleChoice}>
-        <img src={this.props.image} alt={this.props.name} />
-      </button>
-    );
-  }
 }
 
-export default ChoicesImages;
+// class ChoicesImages extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     // this.handleChoice = this.handleChoice.bind(this);
+//   }
+
+//   handleChoice = () => {
+//     const { name } = this.props;
+
+//     const cpuChoice = choices[Math.floor(Math.random() * choices.length)];
+
+//     if (name === cpuChoice) {
+        
+//      return this.props.incrementDraw()
+      
+//     } else if (
+//       (name === "rock" && cpuChoice === "scissors") ||
+//       (name === "paper" && cpuChoice === "rock") ||
+//       (name === "scissors" && cpuChoice === "paper")
+//     ) {
+//      return this.props.incrementUser(name);
+     
+//     } else
+//   return  this.props.incrementCpu(name);
+//   };
+
+//   // Depending on the outcome of the duel, a message needs to be displayed (win? loss? draw?)
+
+//   render() {
+//     return (
+//       <button onClick={this.handleChoice}>
+//         <img src={this.props.image} alt={this.props.name} />
+//       </button>
+//     );
+//   }
+// }
+
+// export default ChoicesImages;
 
